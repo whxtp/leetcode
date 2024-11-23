b7eb7539a5e11c5ac9e67320f928c88554d095a7
MADA0 <mada0s3c@gmail.com>
2024-11-23 16:50:57 +0800

    minimum-consecutive-cards-to-pick-up

--- src/lt2260.java
+++ src/lt2260.java
@@ -1,6 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class lt2260 {
     public int minimumCardPickup(int[] cards) {
-
-        return 0;
+        Map<Integer, Integer> hmap = new HashMap<>();
+        int len = cards.length;
+        int ans = Integer.MAX_VALUE;
+        int now;
+        for (int i = 0; i< len; i++){
+            if (hmap.containsKey(cards[i])){
+                now = i - hmap.get(cards[i]) + 1;
+                hmap.put(cards[i], i);
+                ans = Math.min(now, ans);
+            } else {
+                hmap.put(cards[i], i);
+            }
+        }
+        if(ans == Integer.MAX_VALUE){
+            return -1;
+        }else {
+            return ans;
+        }
     }
 }
